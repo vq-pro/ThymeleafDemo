3025eae7a0c06b2e0ee4ee740bcc0241f7cf99d5
Patrick Robert <pro@virtualite.quebec>
2017-04-03 00:31:42 -0400

    Utils update

--- src/test/java/quebec/virtualite/utils/ui/vo/ViewObjectTest.java
+++ src/test/java/quebec/virtualite/utils/ui/vo/ViewObjectTest.java
@@ -2,11 +2,33 @@ package quebec.virtualite.utils.ui.vo;
 
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNot.not;
 import static org.hamcrest.object.HasToString.hasToString;
 import static org.junit.Assert.assertThat;
 
 public class ViewObjectTest
 {
+    @Test
+    public void equals_whenFalse() throws Exception
+    {
+        assertThat(new ViewObject(1, "A"),
+            not(new ViewObject(1, "B")));
+
+        assertThat(new ViewObject(1, "A"),
+            not(new ViewObject(2, "A")));
+
+        assertThat(new ViewObject(1, "A"),
+            not(new ViewObject(2, "B")));
+    }
+
+    @Test
+    public void equals_whenTrue() throws Exception
+    {
+        assertThat(new ViewObject(1, "A"),
+            is(new ViewObject(1, "A")));
+    }
+
     @Test
     public void toString_verifyOutput() throws Exception
     {
